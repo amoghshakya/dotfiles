0746db9f0c8d73d9866e006fd4d5e638723408a2
Amogh Shakya <amoghshakya@outlook.com>
2025-07-22 14:44:41 +0545

    Extract Neovim config from dotfiles

--- nvim/.config/nvim/lua/core/plugins/bufferline.lua
+++
@@ -1,8 +0,0 @@
-return { -- Tabs
-  "akinsho/bufferline.nvim",
-  version = "*",
-  event = "UIEnter",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  keys = require("core.configs.bufferline").keys,
-  opts = require("core.configs.bufferline").opts,
-}
